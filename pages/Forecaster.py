8529266e125138f7f078c55ad047737c65439bcf
Giorgio Bientinesi <77893973+Giorgiobientinesi@users.noreply.github.com>
2024-10-09 01:41:56 +0200

    Update Forecaster.py

--- pages/Forecaster.py
+++ pages/Forecaster.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime, timedelta
-import openpyxl
-from streamlit import components
 import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
