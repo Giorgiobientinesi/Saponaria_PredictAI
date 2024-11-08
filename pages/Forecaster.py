546e0d2e2ab5a57f6cd93cb97dea175fddbab049
Giorgio Bientinesi <77893973+Giorgiobientinesi@users.noreply.github.com>
2024-11-08 09:32:21 +0100

    Update Forecaster.py

--- pages/Forecaster.py
+++ pages/Forecaster.py
@@ -285,7 +285,7 @@ else:
 
 
     #CREO GRAFICO
-    ultimo_aggiornamento = pd.to_datetime("04/11/2024")
+    ultimo_aggiornamento = pd.to_datetime("10/28/2024")
     # Split the data into actuals and forecasts
     df_actual = previsioni_scopo_grafico[previsioni_scopo_grafico['ds'] <= ultimo_aggiornamento]
     df_forecast = previsioni_scopo_grafico[previsioni_scopo_grafico['ds'] >= ultimo_aggiornamento]
