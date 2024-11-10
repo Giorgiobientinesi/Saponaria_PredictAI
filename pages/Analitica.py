306b1ef03be704f439c96b284c9adb8d8eec2f33
Giorgio Bientinesi <77893973+Giorgiobientinesi@users.noreply.github.com>
2024-11-10 13:19:29 +0100

    Add files via upload

--- pages/Analitica.py
+++ pages/Analitica.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import base64
-
+from Utils import add_logo
 st.set_page_config(layout="wide")
 title_style = """
 <style>
@@ -103,41 +103,6 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-
-def add_logo():
-    # Read and encode the SVG file in base64 format
-    with open("assets/Logo.svg", "rb") as image_file:  # Replace with the actual path
-        svg_base64 = base64.b64encode(image_file.read()).decode("utf-8")
-
-    # Generate the CSS for the sidebar logo
-    st.markdown(
-        f"""
-        <style>
-            [data-testid="stSidebarNav"]::before {{
-                content: "";
-                display: block;
-                margin-left: 20px;
-                margin-top: 20px;
-                width: 150px;
-                height: 150px;
-                background-image: url("data:image/svg+xml;base64,{svg_base64}");
-                background-size: contain;
-                background-repeat: no-repeat;
-                position: relative;
-                top: -10px;
-            }}
-            [data-testid="stSidebarNav"]::after {{
-                content: "";
-                font-size: 35px;
-                font-weight: bold;
-                margin-left: 10px;
-                position: relative;
-                top: -10px;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
 add_logo()
 
