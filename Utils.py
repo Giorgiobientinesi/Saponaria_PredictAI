2e24bbd57dd33ec23e4adb3d0938c6907b2e1408
Giorgio Bientinesi <77893973+Giorgiobientinesi@users.noreply.github.com>
2024-11-13 09:17:53 +0100

    Add files via upload

--- Utils.py
+++ Utils.py
@@ -15,8 +15,8 @@ def add_logo():
                 display: block;
                 margin-left: 20px;
                 margin-top: 20px;
-                width: 300px;
-                height: 150px;
+                width: 100%;
+                height: 50px;
                 background-image: url("data:image/svg+xml;base64,{svg_base64}");
                 background-size: contain;
                 background-repeat: no-repeat;
